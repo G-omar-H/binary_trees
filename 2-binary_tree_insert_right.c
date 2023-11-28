285b48634cac8ad842a9e6de8a71e9f3d532345c
G-omar-H <omarghazi12@gmail.com>
2023-11-28 21:10:12 +0100

    9. Height

--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -26,6 +26,6 @@ binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 	parent->right = new;
 	new->parent = parent;
 	new->right = NULL;
-	new->right = NULL;
+	new->left = NULL;
 	return (new);
 }
--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -7,19 +7,11 @@
  */
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-	size_t h = 0;
+	size_t h_left;
+	size_t h_right;
 
-	if (!tree)
-		return (0);
-	if (tree->left)
-	{
-		h += 1;
-		binary_tree_height(tree->left);
-	}
-	if (tree->right)
-	{
-		h += 1;
-		binary_tree_height(tree->right);
-	}
-	return (h);
+	h_right = tree->right ? 1 + binary_tree_height(tree->right) : 0;
+	h_left = tree->left ? 1 + binary_tree_height(tree->left) : 0;
+
+	return (h_right > h_left ? h_right : h_left);
 }
