7da704cc2e3b7622e9bf4db933436e472c7d04b9
G-omar-H <omarghazi12@gmail.com>
2023-11-28 21:26:34 +0100

    10. Depth

---
+++ 10-binary_tree_depth.c
@@ -0,0 +1,13 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_depth - count node's depth
+ * @tree: pointer to the pointer to count
+ * Return: depth
+ */
+size_t binary_tree_depth(const binary_tree_t *tree)
+{
+	if (!tree)
+		return (0);
+	return tree->parent ? 1 + binary_tree_depth(tree->parent) : 0;
+}
