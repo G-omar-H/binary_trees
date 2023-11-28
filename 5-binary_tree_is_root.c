ef52eeba02eae3a58ee8f6f5d89a79e5592303c8
G-omar-H <omarghazi12@gmail.com>
2023-11-28 18:12:00 +0100

    5. Is root

--- 5-binary_tree_is_root.c
+++ 5-binary_tree_is_root.c
@@ -4,13 +4,13 @@
  * binary_tree_is_root - check if node is root
  * @node: ponter to a node
  * Return: 1 if root, 0 if not
-*/
+ */
 int binary_tree_is_root(const binary_tree_t *node)
 {
-    if (!node)
-        return (0);
+	if (!node)
+		return (0);
 
-    if (!node->parent)
-        return (1);
-    return (0);
+	if (!node->parent)
+		return (1);
+	return (0);
 }
