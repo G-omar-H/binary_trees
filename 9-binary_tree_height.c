6e1ebd462406c9e3f1417bdee2c55403e0519e27
G-omar-H <omarghazi12@gmail.com>
2023-11-28 21:14:18 +0100

    9. Height

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -10,6 +10,8 @@ size_t binary_tree_height(const binary_tree_t *tree)
 	size_t h_left;
 	size_t h_right;
 
+    if (!tree)
+        return (0);
 	h_right = tree->right ? 1 + binary_tree_height(tree->right) : 0;
 	h_left = tree->left ? 1 + binary_tree_height(tree->left) : 0;
 
