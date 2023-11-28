dd26191be9947bde56f66694d46f67ba5f3aa5a2
G-omar-H <omarghazi12@gmail.com>
2023-11-28 18:33:47 +0100

    7. In-order traversal

--- 7-binary_tree_inorder.c
+++ 7-binary_tree_inorder.c
@@ -14,6 +14,6 @@ void binary_tree_inorder(const binary_tree_t *tree, void (*func)(int))
 	if (tree->left)
 		binary_tree_inorder(tree->left, func);
 	func(tree->n);
-	if(tree->right)
+	if (tree->right)
 		binary_tree_inorder(tree->right, func);
 }
